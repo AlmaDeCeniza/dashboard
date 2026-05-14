12bf9250ecb14080537a85204816931221bd48a5
David <dmgarg97@gmail.com>
2026-05-14 11:49:36 -0400

    Reporte añadido

--- dashboard-app/app/__init__.py
+++ dashboard-app/app/__init__.py
@@ -3,17 +3,16 @@ from flask import Flask
 from .extensions import appbuilder, db
 
 
-
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object("config")
 
-    db.init_app(app)        # añadido para inicializar la extensión de SQLAlchemy
+    db.init_app(app)                                # añadido para inicializar la extensión de SQLAlchemy
     with app.app_context():
-        from .models import Categoria, Producto  # Importar los modelos para que SQLAlchemy los reconozca
+        from .models import Categoria, Producto     # Importar los modelos para que SQLAlchemy los reconozca
         db.create_all()
-        appbuilder.init_app(app, db. session)  # añadido para inicializar AppBuilder con la sesión de SQLAlchemy            
-        from . import views  # Importar las vistas para que se registren en AppBuilder
+        appbuilder.init_app(app, db.session)       # añadido para inicializar AppBuilder con la sesión de SQLAlchemy            
+        from . import views                         # Importar las vistas para que se registren en AppBuilder
         
         # Registering the views and APIs
         ...
--- dashboard-app/app/views.py
+++ dashboard-app/app/views.py
@@ -1,9 +1,9 @@
-from .extensions import appbuilder
-from flask_appbuilder import ModelView
+from .extensions import appbuilder, db
+from flask_appbuilder import BaseView, ModelView, expose
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from wtforms import FileField
 from werkzeug.datastructures import FileStorage
-from .models import Categoria, Producto
+from .models import Categoria, Producto, Venta
 
 class CategoriaModelView(ModelView):
     datamodel = SQLAInterface(Categoria)
@@ -59,5 +59,73 @@ class ProductoModelView(ModelView):
         if item.imagen and isinstance(item.imagen, FileStorage):
             item.imagen = item.imagen.read()
     
-appbuilder.add_view(CategoriaModelView, "Categorias", icon="fa-info", category="Configuraciones", category_icon="fa-info")
-appbuilder.add_view(ProductoModelView, "Productos", icon="fa-info", category="Configuraciones", category_icon="fa-info")
+
+class VentaModelView(ModelView):
+    datamodel = SQLAInterface(Venta)
+    """label_columns = { "producto" : "Producto",
+                    "cantidad": "Cantidad",
+                    "precio_unitario" : "Precio Unitario",
+                    "total": "Total",
+                    "fecha": "Fecha"}
+    """
+    list_columns= ["producto", "cantidad", "precio_unitario", "total", "fecha"]
+    add_columns = ["producto", "cantidad", "precio_unitario", "total"]
+    edit_columns = ["producto", "cantidad", "precio_unitario", "total"]
+    #show_columns = ["producto", "cantidad", "precio_unitario", "total", "fecha"]
+
+class ReporteView(BaseView):
+    route_base = '/reportes'
+    @expose("/")
+    def index(self):
+        total_ventas = db.session.query(Venta).count()
+        total_ingresos = db.session.query(
+            db.func.sum(Venta.total)
+        ).scalar() or 0
+        
+        venta_por_producto = db.session.query(
+            Venta.producto,
+            db.func.sum(Venta.cantidad)
+        ).group_by(Venta.producto).all()
+        return self.render_template("reportes.html", 
+                                    t_ventas = total_ventas,
+                                    t_ingresos = total_ingresos,
+                                    venta_por_producto = venta_por_producto
+                                    )
+
+
+
+
+ 
+
+appbuilder.add_view(
+        CategoriaModelView, 
+        "Categorias",
+        icon="fa-info",
+        category="Configuraciones", 
+        category_icon="fa-info"
+    )
+appbuilder.add_view(
+        ProductoModelView, 
+        "Productos", 
+        icon="fa-info", 
+        category="Configuraciones", 
+        category_icon="fa-info"
+    )
+
+appbuilder.add_view(
+        VentaModelView,
+        "ventas",
+        icon="fa-info",
+        category="Ventas",
+        category_icon="fa-shopping-cart"
+    )
+
+
+appbuilder.add_view_no_menu(ReporteView())
+
+appbuilder.add_link(
+    "Reporte1",
+    href="/reportes/",
+    icon="fa-file-text",
+    category="Reportes"
+)
